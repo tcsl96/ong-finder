8e0ed7fe6d2f09d3a388bb4119dc482105d76f0f
Lucas Guilherme <lucasguilherme.devbr@proton.me>
2025-08-20 19:54:17 -0300

    chore: initialize Spring Boot project with Spring Initializr

---
+++ src/main/java/com/ongfinder/ong_finder_api/OngFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.ongfinder.ong_finder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ongfinder/ong_finder_api/OngFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongfinder.ong_finder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
